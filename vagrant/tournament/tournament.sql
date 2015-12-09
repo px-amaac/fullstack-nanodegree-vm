a61f128121c75d1b66f79605cfc72da57a4740e6
Aaron <devaaronmcintyre@gmail.com>
2015-12-08 20:31:39 -0800

    Add gitignore and create database
    I added a gitignore file to ignore all the vagrant files that get changed
    due to ssh. Messed with psql and created the database in tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,6 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-
-
+drop database if exists tournament;
+create database tournament;
+\c tournament;
