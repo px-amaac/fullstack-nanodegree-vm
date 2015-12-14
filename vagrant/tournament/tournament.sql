b7dff44c5bc1dec0ed2be0ab2758faa9abebe79e
Aaron <devaaronmcintyre@gmail.com>
2015-12-13 22:03:30 -0800

    Add players matches and tournament tables
    For a base structure I created some tables with attributes that I thought
    would be useful.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,6 +5,31 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-drop database if exists tournament;
-create database tournament;
+DROP database IF EXISTS tournament;
+CREATE database tournament;
 \c tournament;
+DROP TABLE IF EXISTS matches;
+CREATE TABLE matches
+(
+	round			integer NOT NULL,
+	player_1_id		integer NOT NULL,
+	player_2_id		integer NOT NULL,
+	winner_id		integer
+);
+DROP TABLE IF EXISTS players;
+CREATE TABLE players
+(
+	player_id		integer PRIMARY KEY,
+	name			varchar(45) NOT NULL,
+	wins			integer,
+	opponent_match_wins	integer
+);
+DROP TABLE IF EXISTS tournament;
+CREATE TABLE tournament
+(
+	registered_players_count	integer,
+	total_rounds			integer,
+	rounds_played			integer,
+	matches_played			integer,
+	total_matches			integer
+);
